342a6a9668c33eb27c24afbc8fe02c4fa204cb08
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-04-26 16:28:52 -0300

    .

--- Agenda-react-ATUALIZADO/crud-agenda/src/routes/Dev.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const DEV = () => {
-    return (
-        <div>Página DEV</div>
-    );
-}
-
-export default DEV;
--- Agenda-react-ATUALIZADO/crud-agenda/src/routes/Objetivo.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Objetivo = () => {
-    return (
-        <div>Página Objetivo</div>
-    );
-}
-
-export default Objetivo;
