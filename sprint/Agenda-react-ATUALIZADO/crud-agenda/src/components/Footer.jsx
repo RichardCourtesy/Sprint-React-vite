31fa8f93d83b40aaa6883f752e4b205e3faa2dc5
RichardCourtesy <130692757+RichardCourtesy@users.noreply.github.com>
2024-05-12 17:28:55 -0300

    .

--- sprint/Agenda-react-ATUALIZADO/crud-agenda/src/components/Footer.jsx
+++ sprint/Agenda-react-ATUALIZADO/crud-agenda/src/components/Footer.jsx
@@ -7,9 +7,9 @@ const Footer = () => {
             <div className="Redes">
                 <h1>Redes Sociais</h1>
                 <div className="Rede-icon">
-                    <a href="/">Instagram <i class="bi bi-instagram"></i></a>
-                    <a href="/">Facebook <i class="bi bi-facebook"></i></a>
-                    <a href="/">X  <i class="bi bi-twitter-x"></i></a>
+                    <a href="/">Instagram <i className="bi bi-instagram"></i></a>
+                    <a href="/">Facebook <i className="bi bi-facebook"></i></a>
+                    <a href="/">X  <i className="bi bi-twitter-x"></i></a>
                 </div>
             </div>
 
@@ -19,8 +19,8 @@ const Footer = () => {
 
                 <div className="direito-fim">
                 <div className="Duv-icon">
-                    <a href="/Cursos"><i class="bi bi-book-half"> Mais Cursos</i></a>
-                    <a href="/"> <i class="bi bi-telephone-fill"> Contatos</i></a>
+                    <a href="/Cursos"><i className="bi bi-book-half"> Mais Cursos</i></a>
+                    <a href="/"> <i className="bi bi-telephone-fill"> Contatos</i></a>
                 </div>
 
                 
